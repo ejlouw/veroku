f1f8ab6e91bd372a88c4b15019930184d7ca1f0e
ej_louw <everhard@praelexis.com>
2020-10-24 13:39:05 +0200

    Add wheel requirements.

--- setup.py
+++ setup.py
@@ -20,5 +20,6 @@ setuptools.setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
+    setup_requires=['wheel'],
     install_requires=REQUIREMENTS,
     python_requires='>=3.6')
